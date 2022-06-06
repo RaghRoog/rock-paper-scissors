bab3e6524f1b8fb17de0413fa9ae612d8d6e778d
RaghRoog <szumon612@interia.pl>
2022-06-06 17:53:14 +0200

    done

--- script.js
+++ script.js
@@ -1,47 +1,128 @@
+let playerPoints =0;
+let cpuPoints =0;
+let playerPointsDisplay = document.querySelector('.playerPointsDisplay');
+let cpuPointsDisplay = document.querySelector('.cpuPointsDisplay');
+let result = document.querySelector('.result');
+let computerSelected = document.querySelector('.computer-selected')
+let roundResult = document.querySelector('.round-result');
+playerPointsDisplay.textContent = playerPoints;
+cpuPointsDisplay.textContent = cpuPoints;
 function computerPlay(){
     let number = Math.floor(Math.random()*3);
     
      switch(number){
         case 0:
-            return('Rock')
+            return('ROCK')
             break;
             case 1:
-                return('Paper')
+                return('PAPER')
                 break;
                 case 2:
-                    return('Scissors')
+                    return('SCISSORS')
                     break;
     }
 }
 
-function playRound (playerSelection= prompt('Rock, paper or scissors?'), computerSelection= computerPlay()){
-    console.log(`Computer selected: ${computerSelection}`);
-    if(playerSelection.toUpperCase()===computerSelection.toUpperCase()){
-        console.log('Draw. No one gets a point.');
-    }else if((playerSelection.toUpperCase()==='ROCK'&&computerSelection.toUpperCase()==='SCISSORS')||
-    (playerSelection.toUpperCase()==='PAPER'&&computerSelection.toUpperCase()==='ROCK')||
-    (playerSelection.toUpperCase()==='SCISSORS'&&computerSelection.toUpperCase()==='PAPER')){
-        console.log('You win! ' +playerSelection.toLowerCase() +' beats ' +computerSelection.toLowerCase() +'!');
-        return (playerPoints+=1);
-    }else if((computerSelection.toUpperCase()==='ROCK'&&playerSelection.toUpperCase()==='SCISSORS')||
-    (computerSelection.toUpperCase()==='PAPER'&&playerSelection.toUpperCase()==='ROCK')||
-    (computerSelection.toUpperCase()==='SCISSORS'&&playerSelection.toUpperCase()==='PAPER')){
-        console.log('You lose! ' +computerSelection.toLowerCase() +' beats ' +playerSelection.toLowerCase() +'!');
-        return (cpuPoints+=1);
+let playRoundButton = document.querySelector('.playRoundButton');
+
+let rock = document.querySelector('.rock')
+let paper = document.querySelector('.paper')
+let scissors = document.querySelector('.scissors')
+
+
+let gameEnabled= false;
+
+rock.addEventListener('click', rockPlay);
+paper.addEventListener('click', paperPlay);
+scissors.addEventListener('click', scissorsPlay);
+
+let computerSelection = computerPlay()
+
+function rockPlay(){
+    let rock = document.querySelector('.rock').disabled=true;
+    let paper = document.querySelector('.paper').disabled=true;
+    let scissors = document.querySelector('.scissors').disabled=true;
+     if(computerSelection==='ROCK'){
+         roundResult.textContent='Draw, no one gets a point.';
+     }else if(computerSelection==='SCISSORS'){
+        roundResult.textContent='You win that round.';
+         playerPoints+=1
+     }else{
+        roundResult.textContent='You lose that round.';
+         cpuPoints+=1
+     }
+     if((playerPoints===5)||(cpuPoints===5)){
+        gameSummary();
+        playRoundButton = document.querySelector('.playRoundButton').disabled=true;
+        cpuPointsDisplay.textContent = cpuPoints;
+        playerPointsDisplay.textContent = playerPoints;
+    }
+    computerSelected.textContent = 'Computer selected: ' +computerSelection;
+}
+
+function paperPlay(){
+    let rock = document.querySelector('.rock').disabled=true;
+    let paper = document.querySelector('.paper').disabled=true;
+    let scissors = document.querySelector('.scissors').disabled=true;
+     if(computerSelection==='PAPER'){
+        roundResult.textContent='Draw, no one gets a point.';
+     }else if(computerSelection==='ROCK'){
+        roundResult.textContent='You win that round.';
+         playerPoints+=1
+     }else{
+        roundResult.textContent='You lose that round.';
+         cpuPoints+=1
+     }
+     if((playerPoints===5)||(cpuPoints===5)){
+        gameSummary();
+        playRoundButton = document.querySelector('.playRoundButton').disabled=true;
+        cpuPointsDisplay.textContent = cpuPoints;
+        playerPointsDisplay.textContent = playerPoints;
+    }
+    computerSelected.textContent = 'Computer selected: ' +computerSelection;
+}
+
+function scissorsPlay(){
+    let rock = document.querySelector('.rock').disabled=true;
+    let paper = document.querySelector('.paper').disabled=true;
+    let scissors = document.querySelector('.scissors').disabled=true;
+     if(computerSelection==='SCISSORS'){
+        roundResult.textContent='Draw, no one gets a point.';
+     }else if(computerSelection==='PAPER'){
+        roundResult.textContent='You win that round.';
+         playerPoints+=1
+     }else{
+        roundResult.textContent='You lose that round.';
+         cpuPoints+=1
+     }
+     if((playerPoints===5)||(cpuPoints===5)){
+        gameSummary();
+        playRoundButton = document.querySelector('.playRoundButton').disabled=true;
+        cpuPointsDisplay.textContent = cpuPoints;
+        playerPointsDisplay.textContent = playerPoints;
+    }
+    computerSelected.textContent = 'Computer selected: ' +computerSelection;
+}
+
+
+
+function nextRound(){    
+    cpuPointsDisplay.textContent = cpuPoints;
+    playerPointsDisplay.textContent =playerPoints;
+    let rock = document.querySelector('.rock').disabled=false;
+    let paper = document.querySelector('.paper').disabled=false;
+    let scissors = document.querySelector('.scissors').disabled=false;
+    computerSelection=computerPlay();
+}
+
+playRoundButton.addEventListener('click', nextRound);
+
+function gameSummary(){
+    if(playerPoints>cpuPoints){
+        result.textContent=('You win ' +playerPoints +' to ' +cpuPoints +'.')
+    }else{
+        result.textContent=('You lost ' +cpuPoints +' to ' +playerPoints +'.')
     }
 }
-let playerPoints =0;
-let cpuPoints =0;
-function game(){
-    for(let i = 0; i < 5; i++){
-     playRound()
-    }
-}
-game();
-if(playerPoints>cpuPoints){
-    console.log('You win ' +playerPoints +' to ' +cpuPoints +'.')
-}else if(playerPoints<cpuPoints){
-    console.log('You lose ' +playerPoints + ' to ' +cpuPoints +'.')
-}
-//console.log(playerPoints);
-//console.log(cpuPoints);
+
+
