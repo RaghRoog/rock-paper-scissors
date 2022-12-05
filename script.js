828cc8ec3fc9930cc7e2a7d5be5f2bd65f087937
Szymon Pomieciński <szumon612@interia.pl>
2022-12-05 18:10:28 +0100

    version 2

--- script.js
+++ script.js
@@ -1,128 +1,106 @@
-let playerPoints =0;
-let cpuPoints =0;
+let playerPoints = 0;
+let cpuPoints = 0;
 let playerPointsDisplay = document.querySelector('.playerPointsDisplay');
 let cpuPointsDisplay = document.querySelector('.cpuPointsDisplay');
 let result = document.querySelector('.result');
-let computerSelected = document.querySelector('.computer-selected')
+let computerSelectedDisplayer = document.querySelector('.computer-selected')
 let roundResult = document.querySelector('.round-result');
+let newGameBtn = document.querySelector('.new-game')
+newGameBtn.style.display = 'none'
+let endOfGame = false;
+
 playerPointsDisplay.textContent = playerPoints;
 cpuPointsDisplay.textContent = cpuPoints;
+
 function computerPlay(){
     let number = Math.floor(Math.random()*3);
-    
      switch(number){
         case 0:
             return('ROCK')
             break;
-            case 1:
-                return('PAPER')
-                break;
-                case 2:
-                    return('SCISSORS')
-                    break;
+        case 1:
+            return('PAPER')
+            break;
+        case 2:
+            return('SCISSORS')
+            break;
     }
 }
-
-let playRoundButton = document.querySelector('.playRoundButton');
-
-let rock = document.querySelector('.rock')
-let paper = document.querySelector('.paper')
-let scissors = document.querySelector('.scissors')
-
-
-let gameEnabled= false;
-
-rock.addEventListener('click', rockPlay);
-paper.addEventListener('click', paperPlay);
-scissors.addEventListener('click', scissorsPlay);
 
 let computerSelection = computerPlay()
 
-function rockPlay(){
-    let rock = document.querySelector('.rock').disabled=true;
-    let paper = document.querySelector('.paper').disabled=true;
-    let scissors = document.querySelector('.scissors').disabled=true;
-     if(computerSelection==='ROCK'){
-         roundResult.textContent='Draw, no one gets a point.';
-     }else if(computerSelection==='SCISSORS'){
-        roundResult.textContent='You win that round.';
-         playerPoints+=1
-     }else{
-        roundResult.textContent='You lose that round.';
-         cpuPoints+=1
+let rock = document.getElementById('rock')
+let paper = document.getElementById('paper')
+let scissors = document.getElementById('scissors')
+
+function playerPlayRock(){
+    playerPlay('ROCK')
+}
+function playerPlayPaper(){
+    playerPlay('PAPER')
+}
+function playerPlayScissors(){
+    playerPlay('SCISSORS')
+}
+
+function addListeners(){
+    if(endOfGame == false){
+        rock.addEventListener('click', playerPlayRock);
+        paper.addEventListener('click', playerPlayPaper);
+        scissors.addEventListener('click', playerPlayScissors);
+    }
+    
+}
+addListeners()
+
+function playerPlay(playerSelection){
+    if(computerSelection === playerSelection){
+        roundResult.textContent = 'Draw, no one gets a point.';
+    }else if((computerSelection === 'SCISSORS' && playerSelection === 'ROCK') || 
+            (computerSelection === 'PAPER' && playerSelection === 'SCISSORS') ||
+            (computerSelection === 'ROCK' && playerSelection === 'PAPER')){
+        roundResult.textContent = 'You win that round.';
+        playerPoints += 1
+    }else{
+        roundResult.textContent = 'You lose that round.';
+        cpuPoints += 1
      }
-     if((playerPoints===5)||(cpuPoints===5)){
+    if((playerPoints === 5) || (cpuPoints === 5)){
         gameSummary();
-        playRoundButton = document.querySelector('.playRoundButton').disabled=true;
         cpuPointsDisplay.textContent = cpuPoints;
         playerPointsDisplay.textContent = playerPoints;
     }
-    computerSelected.textContent = 'Computer selected: ' +computerSelection;
-}
-
-function paperPlay(){
-    let rock = document.querySelector('.rock').disabled=true;
-    let paper = document.querySelector('.paper').disabled=true;
-    let scissors = document.querySelector('.scissors').disabled=true;
-     if(computerSelection==='PAPER'){
-        roundResult.textContent='Draw, no one gets a point.';
-     }else if(computerSelection==='ROCK'){
-        roundResult.textContent='You win that round.';
-         playerPoints+=1
-     }else{
-        roundResult.textContent='You lose that round.';
-         cpuPoints+=1
-     }
-     if((playerPoints===5)||(cpuPoints===5)){
-        gameSummary();
-        playRoundButton = document.querySelector('.playRoundButton').disabled=true;
-        cpuPointsDisplay.textContent = cpuPoints;
-        playerPointsDisplay.textContent = playerPoints;
-    }
-    computerSelected.textContent = 'Computer selected: ' +computerSelection;
-}
-
-function scissorsPlay(){
-    let rock = document.querySelector('.rock').disabled=true;
-    let paper = document.querySelector('.paper').disabled=true;
-    let scissors = document.querySelector('.scissors').disabled=true;
-     if(computerSelection==='SCISSORS'){
-        roundResult.textContent='Draw, no one gets a point.';
-     }else if(computerSelection==='PAPER'){
-        roundResult.textContent='You win that round.';
-         playerPoints+=1
-     }else{
-        roundResult.textContent='You lose that round.';
-         cpuPoints+=1
-     }
-     if((playerPoints===5)||(cpuPoints===5)){
-        gameSummary();
-        playRoundButton = document.querySelector('.playRoundButton').disabled=true;
-        cpuPointsDisplay.textContent = cpuPoints;
-        playerPointsDisplay.textContent = playerPoints;
-    }
-    computerSelected.textContent = 'Computer selected: ' +computerSelection;
-}
-
-
-
-function nextRound(){    
+    computerSelectedDisplayer.textContent = 'Computer selected: ' + computerSelection;
     cpuPointsDisplay.textContent = cpuPoints;
-    playerPointsDisplay.textContent =playerPoints;
-    let rock = document.querySelector('.rock').disabled=false;
-    let paper = document.querySelector('.paper').disabled=false;
-    let scissors = document.querySelector('.scissors').disabled=false;
-    computerSelection=computerPlay();
+    playerPointsDisplay.textContent = playerPoints;
+    computerSelection = computerPlay()
 }
 
-playRoundButton.addEventListener('click', nextRound);
+function newGame(){
+    newGameBtn.style.display = 'none'
+    playerPoints = 0
+    cpuPoints = 0
+    endOfGame = false
+    result.innerHTML = ''
+    cpuPointsDisplay.innerHTML = '0'
+    playerPointsDisplay.innerHTML = '0'
+    roundResult.innerHTML = ''
+    computerSelectedDisplayer.innerHTML = ''
+    addListeners()
+}
 
 function gameSummary(){
-    if(playerPoints>cpuPoints){
-        result.textContent=('You win ' +playerPoints +' to ' +cpuPoints +'.')
+    if(playerPoints > cpuPoints){
+        result.textContent = ('You win ' + playerPoints + ' to ' + cpuPoints + '.')
     }else{
-        result.textContent=('You lost ' +cpuPoints +' to ' +playerPoints +'.')
+        result.textContent = ('You lost ' + playerPoints + ' to ' + cpuPoints + '.')
     }
+    endOfGame = true;
+    rock.removeEventListener('click', playerPlayRock)
+    paper.removeEventListener('click', playerPlayPaper)
+    scissors.removeEventListener('click', playerPlayScissors)
+    newGameBtn.style.display = 'block'
+    newGameBtn.addEventListener('click', newGame)
 }
 
 
